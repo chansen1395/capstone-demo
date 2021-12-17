db3305038094057767166d7ee7fcd4b253c258bc
Connor Hansen <chansen13@georgefox.edu>
2021-12-17 13:46:58 -0800

    add demo of end goal.

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,50 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID 
+  }
+
+  class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig);
+        this.auth = app.auth();
+        this.db = app.database();
+    }
+    
+    /*** Authentication  ***/
+    doCreateUserWithEmailAndPassword = (email, password) => 
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) => 
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => 
+        this.auth.signOut();
+
+    doPasswordReset = email => 
+        this.auth.sendPasswordResetEmail(email);
+    
+    /*** Database ***/
+    user = uid => this.db.ref(`users/${uid}`);
+    users = () => this.db.ref('users');
+
+    addActivity = (uid, activity) => {
+        const ref = this.db.ref().child(`users/${uid}/activities`);
+        ref.push(activity);
+    };
+
+    updateActivity = (uid, activity, activityKey) => {
+        const ref = this.db.ref().child(`users/${uid}/activities/${activityKey}`);
+        ref.update(activity);
+    }
+}
+
+export default Firebase;
